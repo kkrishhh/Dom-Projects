35c74c2760a791dafc593e2a496742d1e19fbaad
kkrishhh <soham6218@gmail.com>
2025-07-18 09:56:32 +0530

    completed the first project

--- friend-feature/script.js
+++ friend-feature/script.js
@@ -1,8 +1,34 @@
-var istatus = document.querySelector("#add");
+var a = document.getElementById('add');
+a.addEventListener('click', function(){
+    document.getElementById('status').innerText = "Friend";
+    a.style.backgroundColor = "green";
+    a.style.color = "white";
+    a.style.fontWeight = "700";
+    a.disabled = true;
+    a.innerText = "Friend Added";
+    b.innerText = "Remove";
+    b.style.backgroundColor = "red";
+    b.style.color = "white";
+    b.style.fontWeight = "700";
+    b.disabled = false;
+    h5 = document.getElementById('status');
+    h5.style.color = "green";
+    h5.style.fontWeight = "700";
+})
 
-istatus.addEventListener("click", function(){
-    istatus.innerText = "Added";
-    istatus.style.backgroundColor = "green";
-    istatus.style.color = "white";
-   
-    })
+var b = document.getElementById('remove');
+b.addEventListener('click', function() {
+    document.getElementById('status').innerText = "Stranger";
+    b.style.backgroundColor = "red";
+    b.style.color = "white";
+    b.style.fontWeight = "700";
+    b.disabled = true;
+    b.innerText = "Removed";
+    a.innerText = "Add Friend";
+    a.style.backgroundColor = "cadetblue";
+    a.style.color = "white";
+    a.style.fontWeight = "700";
+    a.disabled = false;
+    h5.style.color = "red";
+    
+})
